418f84bded7859c0b0412588236f121713ec70c9
harshitdayani1996@gmail.com <harshit@coffeebeanstechnologies.com>
2021-07-03 12:25:26 +0530

    Initial Commit.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Initial Commit.
+          Initial Commit..
         </p>
       </header>
     </div>
